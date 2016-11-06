5a5ad58b70cf6da6ebc930bbcd9794c6b174c3a3
“ismailunal” <ismailunal@gmail.com>
2016-11-06 13:31:53 +0000

    Videos with gen.html

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,7 +1,7 @@
 defmodule Rumbl.UserController do
     use Rumbl.Web, :controller
 
-    plug :authenticate when action in [:index, :show]
+    plug :authenticate_user when action in [:index, :show]
 
 
     def index(conn, _params) do
@@ -33,17 +33,4 @@ defmodule Rumbl.UserController do
                 render(conn, "new.html", changeset: changeset)
         end
     end
-
-    defp authenticate(conn, _opts) do
-        if conn.assigns.current_user do
-            conn
-        else
-            conn
-            |> put_flash(:error, "You must be logged in to access that page")
-            |> redirect(to: page_path(conn, :index))
-            |> halt()
-        end
-
-    end
-
 end
